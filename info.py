77f84ed7a5ad4bc0c5f42c6a8a79a79eea259d49
Jokersh200 <165934368+Jokersh200@users.noreply.github.com>
2024-04-15 12:27:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
+URL = environ.get("URL", "https://tall-lillis-sahiljokerorganization.koyeb.app/")
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
